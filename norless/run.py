5fdfafdf4838c171f94a956b61fa17bb653ddf27
Anton Bobrov <bobrov@vl.ru>
2013-08-03 15:51:07 +0400

    run account sync in own thread

--- norless/run.py
+++ norless/run.py
@@ -1,39 +1,44 @@
 import sys
 import os.path
 import argparse
-
 import gdbm as db
+import threading
 
 from mailbox import Maildir, MaildirMessage
 from collections import Counter
 
 from .config import IniConfig
 
+get_maildir_lock = threading.Lock()
+get_state_lock = threading.Lock()
+
 def get_maildir(maildir, cache):
-    try:
-        return cache[maildir]
-    except KeyError:
-        pass
+    with get_maildir_lock:
+        try:
+            return cache[maildir]
+        except KeyError:
+            pass
 
-    result = cache[maildir] = Maildir(
-        os.path.expanduser(maildir), factory=None, create=True)
+        result = cache[maildir] = Maildir(
+            os.path.expanduser(maildir), factory=None, create=True)
 
-    result.name = os.path.basename(maildir)
+        result.name = os.path.basename(maildir)
+        result.store_lock = threading.Lock()
 
-    return result
+        return result
 
 def get_state(config, sync, cache):
-    key = sync.account, sync.folder
-    try:
-        return cache[key]
-    except KeyError:
-        pass
+    with get_state_lock:
+        key = sync.account, sync.folder
+        try:
+            return cache[key]
+        except KeyError:
+            pass
 
-    result = cache[key] = db.open(
-        os.path.join(os.path.expanduser(config.state_dir), '{}.{}'.format(*key)), 'c')
+        result = cache[key] = db.open(
+            os.path.join(os.path.expanduser(config.state_dir), '{}.{}'.format(*key)), 'c')
 
-    return result
-
+        return result
 
 def store_message(maildir, state, uid, message, flags):
     msg = MaildirMessage(message)
@@ -45,12 +50,14 @@ def store_message(maildir, state, uid, message, flags):
         if flags != msg.get_flags():
             oldmessage = maildir[key]
             oldmessage.set_flags(msg.get_flags())
-            maildir[key] = oldmessage
-            maildir.flush()
+            with maildir.store_lock:
+                maildir[key] = oldmessage
     else:
-        state[uid] = maildir.add(msg) + '\n' + msg.get_flags()
+        with maildir.store_lock:
+            key = maildir.add(msg)
+
+        state[uid] = key + '\n' + msg.get_flags()
         state.sync()
-        maildir.flush()
 
 def sync_local(maildir, state, account):
     uid = state.firstkey()
@@ -75,10 +82,8 @@ def sync_local(maildir, state, account):
 
     return maxuid, changes
 
-def sync(config):
-    state_cache = {}
-    maildir_cache = {}
-    for s in config.sync_list:
+def sync_account(config, sync_list, maildir_cache, state_cache):
+    for s in sync_list:
         account = config.accounts[s.account] 
         maildir = get_maildir(s.maildir, maildir_cache)
         state = get_state(config, s, state_cache)
@@ -90,6 +95,24 @@ def sync(config):
         messages = folder.fetch(config.fetch_last, maxuid)
         for m in messages: 
             store_message(maildir, state, m['uid'], m['body'], m['flags'])
+
+def sync(config):
+    state_cache = {}
+    maildir_cache = {}
+    accounts = {}
+    for s in config.sync_list:
+        accounts.setdefault(s.account, []).append(s)
+
+    threads = []
+    for sync_list in accounts.itervalues():
+        t = threading.Thread(target=sync_account,
+            args=(config, sync_list, maildir_cache, state_cache))
+
+        t.start()
+        threads.append(t)
+    
+    for t in threads:
+        t.join()
 
 def check(config):
     maildir_cache = {}
